43e1e9519c5e2c023b02ad25ce2a24137b90e768
Harley <74605804+harley-hutchinson@users.noreply.github.com>
2023-03-16 21:41:12 +0000

    Added more end points

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const express = require("express");
 const fs = require("fs");
-const { uuid } = require("uuidv4");
-const cors = require("cors");
+const { v4: uuid } = require("uuid");
+// const cors = require("cors");
 
 require("dotenv").config();
 
@@ -8,9 +9,126 @@ const PORT = process.env.PORT || 8600;
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.POST.FRONTEND_URL }));
+// app.use(cors({ origin: process.env.POST.FRONTEND_URL }));
 
-app.get("/transactions", (req, res) => {});
+// 1
+app.get("/transactions", (req, res) => {
+  fs.readFile("./data/data.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        Message: "Could not read your transactions from database",
+      });
+    }
+
+    const transactions = JSON.parse(data);
+    res.json(transactions);
+  });
+});
+
+// 2
+app.post("/transactions", (req, res) => {
+  const { title, amount, category, description, isincome } = req.body;
+
+  const newTransactions = {
+    id: uuid(),
+    title: title,
+    amount: amount,
+    category: category,
+    description: description,
+    isincome: isincome,
+  };
+
+  fs.readFile("./data/data.json", (err, data) => {
+    if (err) {
+      return (
+        res.status(500),
+        json({
+          error: true,
+          Message: "Could not read your transactions from database",
+        })
+      );
+    }
+
+    const transactions = JSON.parse(data);
+
+    transactions.push(newTransactions);
+
+    fs.writeFile("./data/data.json", JSON.stringify(transactions), (err) => {
+      if (err) {
+        return res.status(500).json({
+          error: true,
+          Message: "Could not write your transaction(s) from database",
+        });
+      }
+      res.status(201).json(newTransactions);
+    });
+  });
+});
+
+// 3
+// app.delete("/transactions/:transactionsID", (req, res) => {
+//   const transactionsID = req.params.transactionsID;
+
+//   fs.readFile("./data/data.json", (err, data) => {
+//     if (err) {
+//       return res.status(500).json({
+//         error: true,
+//         Message: "Could not read transaction from database",
+//       });
+//     }
+
+//     const transaction = JSON.parse(data);
+
+//     const findTransaction = transaction.find((id) => id.id === transactionsID);
+
+//     fs.writeFile("./data/data.json", JSON.stringify(findTransaction), (err) => {
+//       if (err) {
+//         return res.status(500).json({
+//           error: true,
+//           Message: "Could not updata deleted transaction from database",
+//         });
+//       }
+//       res.status(204).end();
+//     });
+//   });
+// });
+
+app.delete("/transactions/:transactionsID", (req, res) => {
+  const transactionsID = req.params.transactionsID;
+
+  fs.readFile("./data/data.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        Message: "Could not delete transaction from database",
+      });
+    }
+
+    const transactions = JSON.parse(data);
+
+    const transactionIndex = transactions.findIndex((transaction) => transaction.id === transactionsID);
+
+    if (transactionIndex === -1) {
+      return res.status(404).json({
+        error: true,
+        Message: "Transaction not found",
+      });
+    }
+
+    transactions.splice(transactionIndex, 1);
+
+    fs.writeFile("./data/data.json", JSON.stringify(transactions), (err) => {
+      if (err) {
+        return res.status(500).json({
+          error: true,
+          Message: "Could not update deleted transaction from database",
+        });
+      }
+      res.status(204).end();
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
